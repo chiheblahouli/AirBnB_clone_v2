d46b59ecea1019e73171db3e4fcd19dd4b221415
chiheblahouli <chiheb.lahouli@gmail.com>
2020-09-07 11:41:40 +0100

    c

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Flask web application
+"""
+
+
+from flask import Flaskh
+app = Flask(__name__)
+
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
