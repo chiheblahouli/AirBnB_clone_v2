8bb7d46787a5df4dbfb3d6bb05af0f02c84610da
chiheblahouli <chiheb.lahouli@gmail.com>
2020-09-07 12:05:34 +0100

    c

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,9 +14,16 @@ def hello():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)7
 def hbnb():
     """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def Cisfun(text):
+    """print text"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
